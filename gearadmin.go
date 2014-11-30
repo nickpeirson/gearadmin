2c11affb5605bc72ac2f3d6e04c7e94a002540ee
Nick Peirson <nickpeirson@gmail.com>
2014-11-30 00:48:21 +0000

    Initial work on gearadmin library for go

---
+++ gearadmin.go
@@ -0,0 +1,70 @@
+package gearadmin
+
+import (
+	"time"
+	"bufio"
+	"net"
+)
+
+type Client struct {
+	addr string
+	conn net.Conn
+}
+
+func New(host, port string) Client {
+	return Client{addr:host+":"+port}
+}
+
+func (c *Client) Connect() (err error) {
+	if c.conn != nil {
+		return
+	}
+	conn, err := net.DialTimeout("tcp", c.addr, 1*time.Second)
+	if err != nil {
+		return err
+	}
+	c.conn = conn
+	return
+}
+
+func readResponse(conn net.Conn, resp chan string) {
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		if line == ".\n" {
+			break
+		}
+		resp <- line[:len(line)-1]
+	}
+	close(resp)
+}
+
+
+func (c *Client) sendCmd(cmd string) (err error) {
+	if err = c.Connect(); err != nil {
+		return err
+	}
+	writer := bufio.NewWriter(c.conn)
+	writer.WriteString(cmd+"\n")
+	err = writer.Flush()
+	return
+}
+
+func (c *Client) getResponse() (resp []string) {
+	respChan := make(chan string)
+	go readResponse(c.conn, respChan)
+	for line := range respChan {
+		resp = append(resp, line)
+	}
+	return
+}
+
+func (c *Client) Status() (status []string, err error) {
+	if err = c.sendCmd("status"); err != nil {
+		return nil, err
+	}
+	return c.getResponse(), err
+}
---
+++ gearadmin_test.go
@@ -0,0 +1,78 @@
+package gearadmin_test
+
+import (
+	. "github.com/nickpeirson/gearadmin"
+	"testing"
+	"net"
+	"fmt"
+	"bufio"
+	"io/ioutil"
+	"strings"
+)
+
+const (
+	testHost = "localhost"
+	testPort = "49151"
+	testAddr = testHost+":"+testPort
+)
+
+func handleConn(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+	for {
+		cmd, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error", err)
+			continue
+		}
+		resp, err := ioutil.ReadFile("testAssets/"+cmd[:len(cmd)-1]+".txt")
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		_,_ = writer.Write(resp)
+		writer.Flush()
+	}
+}
+
+func mockGearmand() {
+	listener, _ := net.Listen("tcp", testAddr)
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+func init() {
+	go mockGearmand()
+}
+
+func setupClient() Client {
+	return New(testHost, testPort)
+}
+
+func TestCanConnect(t *testing.T) {
+	c := setupClient()
+	if err := c.Connect(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestCanGetStatus(t *testing.T) {
+	c := setupClient()
+	got, err := c.Status()
+	if err != nil {
+		t.Error(err)
+	}
+	want, err := ioutil.ReadFile("testAssets/status.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	if got[0] != string(want[:strings.IndexByte(string(want),'\n')]) {
+		t.Errorf("got %#v want %#v", got[0], string(want[:strings.IndexByte(string(want),'\n')]))
+	}
+}
